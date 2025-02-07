98c71d0d705aa8491e7e3dea02936ea3fadd0174
Your Name <your.email@example.com>
2025-02-07 10:33:30 -0600

    Yaml file

--- MvcCrudApp/obj/Debug/net9.0/MvcCrudApp.AssemblyInfo.cs
+++ MvcCrudApp/obj/Debug/net9.0/MvcCrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59779e116b03edd1b8e8d02a8a38b1e9f472ef1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
