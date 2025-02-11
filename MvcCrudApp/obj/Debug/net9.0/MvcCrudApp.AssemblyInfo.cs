2e7d2efb580bc38043584fd212b5c94d60c7c037
Your Name <your.email@example.com>
2025-02-10 19:14:32 -0600

    Initial commit - ASP.NET MVC CRUD App

--- MvcCrudApp/obj/Debug/net9.0/MvcCrudApp.AssemblyInfo.cs
+++ MvcCrudApp/obj/Debug/net9.0/MvcCrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8271d6e38e71b9e32bac1175bf55147c2b0c6f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912c739f96f7b7e1499ab18b70e868a36da7f103")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
